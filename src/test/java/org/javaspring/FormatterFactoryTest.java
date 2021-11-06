71a58260739f6a7dcb3f1398a9bfd90f84f34635
Roman Koshelev <Leo_dialog@mail.ru>
2021-11-06 18:03:40 +0300

    Added tests for FormatterFactory.java

---
+++ src/test/java/org/javaspring/FormatterFactoryTest.java
@@ -0,0 +1,31 @@
+package org.javaspring;
+
+import org.javaspring.task4.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FormatterFactoryTest {
+
+    @Test
+    void shouldReturnStandardFormatter() {
+        Formatter formatter = new FormatterFactory().getFormatter(TypeOfFormatters.STANDARD.toString());
+        assertTrue(formatter instanceof StandardFormatter);
+    }
+
+    @Test
+    void shouldReturnSimpleFormatter() {
+        Formatter formatter = new FormatterFactory().getFormatter(TypeOfFormatters.SIMPLE.toString());
+        assertTrue(formatter instanceof SimpleFormatter);
+    }
+
+    @Test
+    void shouldReturnException() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new FormatterFactory().getFormatter("neverExistedType");
+        });
+
+        assertTrue(thrown.getMessage().contains("Type of formatter does not exist!"));
+    }
+}
